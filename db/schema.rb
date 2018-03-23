3643b9ba938a7544752724a1a21b65439c348500
claudemirmendes <avelinopopping@gmail.com>
2018-03-23 16:10:34 -0300

    Adding migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180322233755) do
+ActiveRecord::Schema.define(version: 20180323190749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "survivor_id"
+  end
 
   create_table "survivors", force: :cascade do |t|
     t.string "name"
@@ -23,6 +31,7 @@ ActiveRecord::Schema.define(version: 20180322233755) do
     t.integer "long"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "infected"
   end
 
 end
