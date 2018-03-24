03856ea3d3c757feedc4fd59de3d5d5e0e67efe3
claudemirmendes <avelinopopping@gmail.com>
2018-03-24 17:31:38 -0300

    remove byebug

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,6 @@ class Item < ApplicationRecord
   enum type: [ :agua, :alimento, :medicacao, :municao ]
 
   def self.create_item(item,inventory_id)
-  	byebug
   	@item = Item.create(name: item["item"],
                         inventory_id: inventory_id,
                         tipo: item["tipo"]
